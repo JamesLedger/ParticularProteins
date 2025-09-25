1bb16a82aeabcf56407a59700a2cd83ceef6ea62
James Ledger <Jamesledger99@gmail.com>
2025-09-25 23:23:53 +0100

    select atom and highlight

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import coordinates from "./lib/cifParsing/coordinates.json";
 import Demo from "./components/3d/demo";
+import { useState } from "react";
+
+function symbolToName(id: string): string {
+  switch (id) {
+    case "N":
+      return "Nitrogen";
+    case "C":
+      return "Carbon";
+    case "O":
+      return "Oxygen";
+    case "S":
+      return "Sulfur";
+    case "H":
+      return "Hydrogen";
+    case "P":
+      return "Phosphorus";
+  }
+  return id;
+}
 
 function App() {
+  const [selected, setSelected] = useState<Coordinate | null>(null);
+
   return (
-    <div className="w-full flex justify-center">
-      <Card className="w-xl">
+    <div className="w-full flex flex-col justify-center gap-4">
+      <h1 className="mb-0 text-balance font-medium text-5xl tracking-tighter!">
+        Particular Proteins
+      </h1>
+      <Card>
         <CardHeader>
-          <CardTitle>Particular Proteins</CardTitle>
+          <CardTitle>1XPB.cif</CardTitle>
           <CardDescription>
-            Currently displaying the coordinates of the protein 1XPB.cif in 3D
+            Red is oxygen, blue is nitrogen, green is carbon
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <Demo />
-          Raw coordinates data:
-          <div className="max-h-96 overflow-y-auto bg-gray-100">
-            <pre>{JSON.stringify(coordinates, null, 2)}</pre>
-          </div>
+        <CardContent className="w-full h-[500px] mb-2">
+          <Demo onSelect={setSelected} />
+
+          {selected && (
+            <div className="mb-2 pb-2 text-sm text-gray-600 ">
+              <span className="font-medium">Selected:</span>
+              {symbolToName(selected.element)}
+            </div>
+          )}
         </CardContent>
       </Card>
     </div>
--- src/components/3d/demo.tsx
+++ src/components/3d/demo.tsx
@@ -1,23 +1,37 @@
 import { Canvas } from "@react-three/fiber";
-import { CameraControls } from "@react-three/drei";
+import { CameraControls, Outlines } from "@react-three/drei";
 import coordinates from "../../lib/cifParsing/coordinates.json";
+import { useState } from "react";
 
-function Demo() {
+function Demo({
+  onSelect,
+}: Readonly<{ onSelect?: (coord: Coordinate) => void }>) {
   return (
-    <div>
-      <Canvas style={{ background: "white", width: "500px", height: "500px" }}>
-        <CameraControls makeDefault />
-        <ambientLight />
+    <Canvas
+      style={{ background: "white", width: "100%", height: "100%" }}
+      camera={{ position: [-10, 0, 50] }}
+    >
+      <CameraControls />
+      <ambientLight />
 
-        {coordinates.map((coord) => (
-          <Element key={coord.element} coords={coord} />
-        ))}
-      </Canvas>
-    </div>
+      {coordinates.map((coord, idx) => (
+        <Element
+          key={`${coord.element}-${idx}`}
+          coords={coord}
+          onSelect={onSelect}
+        />
+      ))}
+    </Canvas>
   );
 }
 
-function Element({ coords }: { coords: Coordinate }) {
+function Element({
+  coords,
+  onSelect,
+}: Readonly<{
+  coords: Coordinate;
+  onSelect?: (coord: Coordinate) => void;
+}>) {
   const colour = (element: string) => {
     switch (element) {
       case "N":
@@ -33,12 +47,27 @@ function Element({ coords }: { coords: Coordinate }) {
     }
   };
 
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <mesh scale={0.1} position={[coords.x, coords.y, coords.z]}>
+    <mesh
+      scale={0.2}
+      position={[coords.x, coords.y, coords.z]}
+      onClick={() => onSelect?.(coords)}
+      onPointerEnter={() => {
+        setHovered(true);
+      }}
+      onPointerLeave={() => {
+        setHovered(false);
+      }}
+    >
       <sphereGeometry />
 
       {/* The colour of the sphere is based on the element */}
       <meshBasicMaterial color={colour(coords.element)} />
+      {hovered && (
+        <Outlines screenspace={true} thickness={0.5} color="orange" />
+      )}
     </mesh>
   );
 }
